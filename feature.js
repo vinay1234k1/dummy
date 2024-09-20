4cfc2ceef3d9cf244a8beec0c3e2b1688a337759
vinay1234k1 <vinay.kumar@codttech.com>
2024-09-20 14:07:57 +0530

    second commit

--- feature.js
+++ feature.js
@@ -1,7 +1,3 @@
-sgbdgbavgvsagvsywgqvghw ghxgavfc ghsa bhsbvhc ugs xyvwvyvhb b xajhjVB BH VVCFX GHA GH AV GVvgVDV V  vb gcvgh GC WVYWQ V GSCBHA  GAVGVGA DGVYGHA BH DAVBVGHCVGVSVGSVV  VHW
+console.log("aman");
 
-
-CJANNJ
-
-SAJUNFIUNQ
-NNJANJ
+console .log("vinay");
